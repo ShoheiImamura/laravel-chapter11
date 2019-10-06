99790615c29d6293d5029628a1542109bf421d65
ShoheiImamura <s3imamura@gmail.com>
2019-10-06 11:26:43 +0900

    factory 実装

--- tdd_sample/database/factories/ReportFactory.php
+++ tdd_sample/database/factories/ReportFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Report::class, function (Faker $faker) {
     return [
-        //
+        "visit_date" => $faker->date(),
+        "detail" => $faker->realText(),
     ];
 });
